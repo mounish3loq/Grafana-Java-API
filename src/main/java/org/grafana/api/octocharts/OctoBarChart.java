4b1cefa1d2c851b2540f372c2a2145ac36312c80
mounish3loq <mounish.doli@3loq.com>
2020-04-23 13:04:04 +0530

    Added changes to enable appending of panels to existing Dashboards.

--- src/main/java/org/grafana/api/octocharts/OctoBarChart.java
+++ src/main/java/org/grafana/api/octocharts/OctoBarChart.java
@@ -1,6 +1,7 @@
 package org.grafana.api.octocharts;
 
 import org.grafana.api.GrafanaAPI;
+import org.grafana.api.responses.Dashboard.DashboardRsp;
 import org.grafana.api.responses.Dashboard.NewCreateUpdateDashboardRsp;
 import org.grafana.api.templates.Charts.PlotlyPanelChart;
 import org.grafana.api.templates.Dashboard.CreateUpdateDashboardTpl;
@@ -27,10 +28,17 @@ public class OctoBarChart {
         String grafanaserver = "http://localhost:3000";
         String mainOrgApiKey = "Bearer eyJrIjoiSmtSNUY2R3RyV0hVQ0oxQ0E5NlJlZ0lXYVp4Z0s0T1QiLCJuIjoiVGVzdCBLZXkiLCJpZCI6MX0= ";
         GrafanaAPI grafanaAPI = new GrafanaAPI(grafanaserver);
+        DashboardTpl dashItems;
+        DashboardRsp dashboardRsp = grafanaAPI.orgAdminAPI(mainOrgApiKey).getDashboardByUid(this.uid);
+        if (dashboardRsp == null){
+            dashItems = new DashboardTpl();
+            dashItems.setUid(this.uid);
+            dashItems.setTitle("MyTestTitle2");
+        }else{
+            dashItems = dashboardRsp.getDashboard();
+        }
         CreateUpdateDashboardTpl dashTest = new CreateUpdateDashboardTpl();
-        DashboardTpl dashItems = new DashboardTpl();
         dashItems.setPanels(this.barpanel);
-        dashItems.setTitle("MyTestTitle2");
         dashTest.setDashboard(dashItems);
         NewCreateUpdateDashboardRsp createUpdateDashboard = grafanaAPI.orgAdminAPI(mainOrgApiKey).createUpdateDashboard(dashTest);
     }
