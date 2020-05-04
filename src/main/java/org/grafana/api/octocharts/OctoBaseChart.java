84923d60ba86e27fe85d89597e4b5d94b8f3771a
saurabh <saurabh@3loq.com>
2020-05-04 14:27:52 +0530

    updating logger.

--- src/main/java/org/grafana/api/octocharts/OctoBaseChart.java
+++ src/main/java/org/grafana/api/octocharts/OctoBaseChart.java
@@ -1,13 +1,15 @@
 package org.grafana.api.octocharts;
+
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.functions;
-import org.grafana.api.GrafanaAPI;
-import org.grafana.api.responses.Dashboard.DashboardRsp;
-import org.grafana.api.responses.Dashboard.NewCreateUpdateDashboardRsp;
-import org.grafana.api.templates.Dashboard.CreateUpdateDashboardTpl;
-import org.grafana.api.templates.Dashboard.DashboardTpl;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.functions;
+import org.apache.spark.sql.SparkSession;
+
+import org.grafana.api.GrafanaAPI;
+import org.grafana.api.templates.Dashboard.DashboardTpl;
+import org.grafana.api.responses.Dashboard.DashboardRsp;
+import org.grafana.api.templates.Dashboard.CreateUpdateDashboardTpl;
+import org.grafana.api.responses.Dashboard.NewCreateUpdateDashboardRsp;
 import org.grafana.api.templates.Dashboard.abstractbasepanel.BasePanelTpl;
 
 import java.util.Properties;
@@ -15,14 +17,17 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public abstract class OctoBaseChart {
-    static Logger log = Logger.getLogger(OctoBaseChart.class.getName());
+    //static Logger log = Logger.getLogger(OctoBaseChart.class.getName());
+    static Logger log = Logger.getLogger("myLogger");
     public void updateChartData(SparkSession spark,Dataset<Row> df,String dashboarduid,String workunitname,String summaryname){
+        String tableName = workunitname.substring(workunitname.lastIndexOf('.') + 1) +"_"+ summaryname;
         log.info("UpdateChartData "+" Spark Session Id: " + spark +" Table Name: " + workunitname+"_"+summaryname);
 
-        String tableName = workunitname +"_"+ summaryname;
         Properties connectionProperties = new Properties();
         connectionProperties.put("user", System.getenv("GRAFANA_POSTGRES_USERNAME"));
         connectionProperties.put("password", System.getenv("GRAFANA_POSTGRES_PASSWORD"));
+        connectionProperties.put("driver", "org.postgresql.Driver");
+
         df = df.withColumn("dashboardid",functions.lit(dashboarduid));
         df = df.withColumn("workunitname", functions.lit(workunitname));
         df = df.withColumn("summaryname",functions.lit(summaryname));
@@ -30,12 +35,11 @@ public abstract class OctoBaseChart {
                 .mode("append")
                 .jdbc("jdbc:postgresql://"+System.getenv("GRAFANA_POSTGRES_URL")+"/"+System.getenv("GRAFANA_POSTGRES_DB"), tableName, connectionProperties);
     }
-  
+
     public void publish(String uid, String dashboardtitle, BasePanelTpl panel){
         log.info("Method Name : BasePanelTpl Publish"+"Uid : "+uid + "Dashboard Title : "+dashboardtitle);
 
         String grafanaserver = System.getenv("GRAFANA_SERVER");
-//        "Bearer eyJrIjoiSmtSNUY2R3RyV0hVQ0oxQ0E5NlJlZ0lXYVp4Z0s0T1QiLCJuIjoiVGVzdCBLZXkiLCJpZCI6MX0= "
         String mainOrgApiKey = System.getenv("GRAFANA_APIKEY");
         GrafanaAPI grafanaAPI = new GrafanaAPI(grafanaserver);
         DashboardTpl dashItems;
