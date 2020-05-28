acfb8c0d4708cf93697b26318e1320795ef75e13
saurabh <saurabh@3loq.com>
2020-05-28 09:14:23 +0530

    updating with new api.

--- src/main/java/org/grafana/api/driver/OctoExample.java
+++ src/main/java/org/grafana/api/driver/OctoExample.java
@@ -31,22 +31,22 @@ public class OctoExample {
         Dataset<Row> df3 =spark.read()
                 .jdbc("jdbc:postgresql://"+System.getenv("GRAFANA_POSTGRES_URL")+"/"+System.getenv("GRAFANA_POSTGRES_DB"), "sample_heatmap_table", connectionProperties);
 
-        OctoLineChart octoLineChart = new OctoLineChart(spark,"ABCDE",df2,"Lineworkunit","LineSummary","Line chart");
+        OctoLineChart octoLineChart = new OctoLineChart(spark,"ABCDE",df2,"","Lineworkunit","LineSummary","Line chart");
         octoLineChart.setTimeColumn("year_month");
         octoLineChart.setColumns("varejo AS \"Varejo\",vestuario AS \"Vestuario\",\"serviÇo\" AS \"Serviço\",supermercados AS \"Supermercados\",restaurante AS \"Restaurante\", \"posto_de_gas\" AS \"Posto De Gas\"");
         octoLineChart.publish();
 
-        OctoBarChart octoBarChart = new OctoBarChart(spark,"ABCDE",df,"Sampleworkunit","SampleSummary","xdata","ydata","Bar chart");
+        OctoBarChart octoBarChart = new OctoBarChart(spark,"ABCDE",df,"", "Sampleworkunit","SampleSummary","xdata","ydata","Bar chart");
         octoBarChart.setTrace("a","b");
         octoBarChart.publish();
 
-        OctoHeatmapChart octoHeatmapChart = new OctoHeatmapChart(spark,"ABCDE",df3,"Heatworkunit","HeatSummary","xdata","ydata","Heatmap chart");
+        OctoHeatmapChart octoHeatmapChart = new OctoHeatmapChart(spark,"ABCDE",df3,"", "Heatworkunit","HeatSummary","xdata","ydata","Heatmap chart");
         octoHeatmapChart.setTrace("category","category","i0");
         octoHeatmapChart.setTarget("category");
         octoHeatmapChart.setTarget("i0,i1,i2,i3,i4");
         octoHeatmapChart.publish();
 
-        OctoTableChart octoTableChart = new OctoTableChart(spark,"ABCDE",df3,"Tableworkunit","TableSummary","Table chart");
+        OctoTableChart octoTableChart = new OctoTableChart(spark,"ABCDE",df3, "", "Tableworkunit","TableSummary","Table chart");
         octoTableChart.setColumns("category,i0");
         octoTableChart.publish();
 
