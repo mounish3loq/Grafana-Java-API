26c10dd831ee6970124be3c9ed6036c341249e30
saurabh <saurabh@3loq.com>
2020-05-28 09:15:35 +0530

    adding workunitName to differentiate multiple wu in same blueprint.

--- src/main/java/org/grafana/api/octocharts/OctoTableChart.java
+++ src/main/java/org/grafana/api/octocharts/OctoTableChart.java
@@ -11,27 +11,29 @@ public class OctoTableChart extends OctoBaseChart {
     private String dashboardtitle;
     private String tableName;
     private String columns;
+    private String workunitClass;
     private String workunitName;
     public TablePanelTpl tablepanel;
 
-    public OctoTableChart(SparkSession spark, String dashboarduid, Dataset<Row> df, String workunitname, String summaryname, String paneltitle){
+    public OctoTableChart(SparkSession spark, String dashboarduid, Dataset<Row> df, String workunitClass, String workunitname, String summaryname, String paneltitle){
         this.dashboarduid = dashboarduid;
         this.dashboardtitle = null;
         this.tablepanel = new TablePanelTpl();
         this.tablepanel.setDatasource(System.getenv("GRAFANA_POSTGRES_DATASOURCE"));
         this.tablepanel.setTitle(paneltitle);
         this.tablepanel.setType("table");
+        this.workunitClass = workunitClass;
         this.workunitName = workunitname;
-        this.tableName=workunitname.substring(workunitname.lastIndexOf('.') + 1) +"_"+ summaryname;
-        this.updateChartData(spark,df,dashboarduid,workunitname,summaryname);
+        this.tableName=(workunitClass.substring(workunitClass.lastIndexOf('.') + 1) +"_"+ summaryname).toLowerCase();
+        this.updateChartData(spark,df,dashboarduid,workunitClass,workunitName,summaryname);
     }
     public void setColumns(String cols){
         this.columns = cols;
     }
     public void publish(){
-        String query = String.format("SELECT\n %s FROM %s where dashboardid = \'%s\'",this.columns,this.tableName,this.dashboarduid);
+        String query = String.format("SELECT\n %s FROM %s where dashboardid = \'%s\' and workunitname = \'%s\'",this.columns,this.tableName,this.dashboarduid, this.workunitName);
         this.tablepanel.setTargets(query);
-        super.publish(this.dashboarduid,this.dashboardtitle,this.tablepanel,this.workunitName);
+        super.publish(this.dashboarduid,this.dashboardtitle,this.tablepanel,this.workunitClass);
     }
     public void setDashboardtitle(String dashboardtitle){
         this.dashboardtitle = dashboardtitle;
